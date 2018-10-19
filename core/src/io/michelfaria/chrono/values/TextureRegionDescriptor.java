505dd84ae9e1b823625a4cc3d87ab766ac84cb28
Michel <michelfariarj@gmail.com>
2018-10-19 12:03:23 -0300

    Flip support for Crono

--- core/src/io/michelfaria/chrono/values/TextureRegionDescriptor.java
+++ core/src/io/michelfaria/chrono/values/TextureRegionDescriptor.java
@@ -1,37 +1,45 @@
 package io.michelfaria.chrono.values;
 
+import io.michelfaria.chrono.values.TextureRegionDescriptor.FlipData;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+
+import static io.michelfaria.chrono.values.TRDConstants.*;
+
+class TRDConstants {
+    public static final byte[] flipHorz = {FlipData.FLIP_HORZ};
+    public static final int[] arr0 = {0};
+    public static final FlipData flipHorizontal = new FlipData(arr0, flipHorz);
+    public static final int[] arr0_1_0_2 = {0, 1, 0, 2};
+}
 
 public enum TextureRegionDescriptor {
 
     // @formatter:off
     CRONO_IDLE_NORTH("crono-idle-north", 1, 1),
     CRONO_IDLE_SOUTH("crono-idle-south", 1, 1),
-    CRONO_IDLE_WEST("crono-idle-west", 1, 1),
+    CRONO_IDLE_WEST("crono-idle-east", 1, 1, 0, null, flipHorizontal),
     CRONO_IDLE_EAST("crono-idle-east", 1, 1),
     CRONO_WALK_NORTH("crono-walk-north", 6, 1, 0.125f),
     CRONO_WALK_SOUTH("crono-walk-south", 6, 1, 0.125f),
-    CRONO_WALK_WEST("crono-walk-west", 6, 1, 0.125f),
+    CRONO_WALK_WEST("crono-walk-east", 6, 1, 0.125f, null, flipHorizontal),
     CRONO_WALK_EAST("crono-walk-east", 6, 1, 0.125f),
     CRONO_RUN_NORTH("crono-run-north", 6, 1, 0.1f),
     CRONO_RUN_SOUTH("crono-run-south", 6, 1, 0.1f),
-    CRONO_RUN_WEST("crono-run-west", 6, 1, 0.1f),
+    CRONO_RUN_WEST("crono-run-east", 6, 1, 0.1f, null, flipHorizontal),
     CRONO_RUN_EAST("crono-run-east", 6, 1, 0.1f),
 
     UI_DIALOGBOX_0("ui-dialogbox-0", 1, 1),
 
-    NU_IDLE_NORTH("nu-walk-north", 3, 1, 0.3f, new int[]{0}),
-    NU_IDLE_SOUTH("nu-walk-south", 3, 1, 0.3f, new int[]{0}),
-    NU_IDLE_WEST("nu-walk-east", 3, 1, 0.3f, new int[]{0},
-            new FlipData(new int[]{0}, new byte[]{FlipData.FLIP_HORZ})),
-    NU_IDLE_EAST("nu-walk-east", 3, 1, 0.3f, new int[]{0}),
+    NU_IDLE_NORTH("nu-walk-north", 3, 1, 0.3f, arr0),
+    NU_IDLE_SOUTH("nu-walk-south", 3, 1, 0.3f, arr0),
+    NU_IDLE_WEST("nu-walk-east", 3, 1, 0.3f, arr0, flipHorizontal),
+    NU_IDLE_EAST("nu-walk-east", 3, 1, 0.3f, arr0),
 
-    NU_WALK_NORTH("nu-walk-north", 3, 1, 0.3f, new int[]{0, 1, 0, 2}),
-    NU_WALK_SOUTH("nu-walk-south", 3, 1, 0.3f, new int[]{0, 1, 0, 2}),
-    NU_WALK_WEST("nu-walk-east", 3, 1, 0.3f, new int[]{0, 1, 0, 2},
-            new FlipData(new int[]{0}, new byte[]{FlipData.FLIP_HORZ})),
-    NU_WALK_EAST("nu-walk-east", 3, 1, 0.3f, new int[]{0, 1, 0, 2});
+    NU_WALK_NORTH("nu-walk-north", 3, 1, 0.3f, arr0_1_0_2),
+    NU_WALK_SOUTH("nu-walk-south", 3, 1, 0.3f, arr0_1_0_2),
+    NU_WALK_WEST("nu-walk-east", 3, 1, 0.3f, arr0_1_0_2, flipHorizontal),
+    NU_WALK_EAST("nu-walk-east", 3, 1, 0.3f, arr0_1_0_2);
     // @formatter:on
 
     public final String regionName;
